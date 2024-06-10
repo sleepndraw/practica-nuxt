bba1b2e4a4c16010f00b2db6c70dba2cf5e6fb5e
sleepndraw <thepikahouse@gmail.com>
2024-06-10 12:24:04 +0200

    aos fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,11 +12,9 @@ export default {
     ]
   },
   pageTransition: {
-  
     mode: 'out-in'
   },
   layoutTransition: {
-  
     mode: 'out-in'
   },
   // Global CSS
@@ -35,7 +33,7 @@ export default {
   
   aos: {
     // Global settings:
-    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    disable: 'mobile', // disables AOS on mobile devices
     startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
     initClassName: 'aos-init', // class applied after initialization
     animatedClassName: 'aos-animate', // class applied on animation
@@ -44,7 +42,6 @@ export default {
     debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
     throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
     
-
     // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
     offset: 400, // offset (in px) from the original trigger point
     delay: 15, // values from 0 to 3000, with step 50ms
@@ -53,24 +50,24 @@ export default {
     once: false, // whether animation should happen only once - while scrolling down
     mirror: false, // whether elements should animate out while scrolling past them
     anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-    },
+  },
+  
   // Auto import components
   components: true,
 
   // Modules for dev and build
   buildModules: [
     // Add your build modules here
-
   ],
 
   // Modules
-  modules: 
-    [// Add your build modules here
-    '@nuxtjs/tailwindcss', '@nuxtjs/google-fonts', 'nuxt-icon', "@nuxt/image", 'nuxt-aos'],
+  modules: [
+    // Add your build modules here
+    '@nuxtjs/tailwindcss', '@nuxtjs/google-fonts', 'nuxt-icon', "@nuxt/image", 'nuxt-aos'
+  ],
   
-  
-  googleFonts:{
-    families:{
+  googleFonts: {
+    families: {
       Archivo: true,
     },
   },
@@ -79,9 +76,7 @@ export default {
   build: {
     // Add your build configurations here
     postcss: {
-      postcssOptions: require('./postcss.config.js',),
-     
+      postcssOptions: require('./postcss.config.js'),
     },
   },
-  
 }
